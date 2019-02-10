abb4cddb5aca4bbaf78ee0cc8a51a2b8e4db1273
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-02-09 19:49:33 -0500

    Final audio file!

--- Assignment1/part2/part2.py
+++ Assignment1/part2/part2.py
@@ -6,13 +6,11 @@ Created on Sat Feb  2 14:35:53 2019
 """
 #%%
 import torch.nn as nn
-#%%
 import librosa
 import numpy as np
 import torch
 
-print(torch.cuda.is_available())
-
+#%%
 s, sr=librosa.load('data/train_clean_male.wav', sr=None)
 S=librosa.stft(s, n_fft=1024, hop_length=512)
 sn, sr=librosa.load('data/train_dirty_male.wav', sr=None)
@@ -22,10 +20,8 @@ X=librosa.stft(sn, n_fft=1024, hop_length=512)
 s_abs = np.abs(S)
 x_abs = np.abs(X)
 #%%
-s = torch.tensor(s_abs)
-x = torch.tensor(x_abs)
-s = s.cuda()
-x = x.cuda()
+s = torch.tensor(np.transpose(s_abs)).cuda()
+x = torch.tensor(np.transpose(x_abs)).cuda()
 BATCH = 171
 
 #%%
@@ -36,36 +32,38 @@ test_loader = torch.utils.data.DataLoader(s, batch_size=BATCH)
 class NeuralNet(nn.Module):
   def __init__(self):
     super(NeuralNet, self).__init__()
-    self.input_layer = nn.Linear(2459, 2459)
+    self.input_layer = nn.Linear(513, 2459)
     self.hidden_layer1 = nn.Linear(2459, 2459)
     self.hidden_layer2 = nn.Linear(2459, 2459)
-    self.output_layer = nn.Linear(2459, 2459)
+    self.output_layer = nn.Linear(2459, 513)
     
     self.activation = nn.ReLU()
 
   def forward(self, data):
-
-    data = data.view(-1, 2459).cuda()
-    print('data shape')
     print(data.shape)
+    data = data.view(-1, 513).cuda()
     output = self.activation(self.input_layer(data))
     output = self.activation(self.hidden_layer1(output))
     output = self.activation(self.hidden_layer2(output))
     output_layer = self.output_layer(output)
-    final = torch.nn.functional.relu(output_layer, dim=1)
-    print(final.shape)
+    final = torch.nn.functional.relu(output_layer)
     return final
+
+
 #%%
 neural_network = NeuralNet()
 neural_network = neural_network.cuda()
-loss_function = nn.CrossEntropyLoss()
+#network_output , torch.max(target_set, 1)[1]
+# def loss_function(ypred,yactual):
+#   return torch.sum((ypred - yactual) ** 2)
+loss_function= nn.MSELoss()
 para = neural_network.parameters()
 optimizer = torch.optim.Adam(params=para, lr=0.01)
 
 #%%
 def get_hadamard_product_result(x_test, x_test_abs, s_hat_test):
     div = x_test / x_test_abs
-    return div * s_hat_test
+    return torch.tensor(div) * s_hat_test
 
 #%%
 input_iter = iter(train_loader)
@@ -78,12 +76,9 @@ for i in range(s.shape[0]//BATCH):
     
     input_set = input_set.cuda()
     target_set = target_set.cuda()
-    
-#    target_set = target_set.view(-1, 2459)
     optimizer.zero_grad()
     network_output  = neural_network(input_set)
-#    target_set = target_set.long()
-    loss = loss_function(network_output , torch.max(target_set, 1)[1])
+    loss = loss_function(network_output , target_set)
     loss.backward()
     optimizer.step()
     
@@ -91,28 +86,32 @@ for i in range(s.shape[0]//BATCH):
 st, sr=librosa.load('data/test_x_01.wav', sr=None)
 test_01 = librosa.stft(st, n_fft=1024, hop_length=512)
 test_01_abs = torch.tensor(np.abs(test_01))
-test_01_abs = test_01_abs.cuda()
-
-t_loader = torch.utils.data.DataLoader(test_01_abs, batch_size=513)
+test_01_abs = np.transpose(test_01_abs)
+# test_01 = torch.tensor(test_01)
+t_loader = torch.utils.data.DataLoader(test_01_abs, batch_size=142)
 #%%  
 with torch.no_grad():
-    for i, data in enumerate(t_loader):
+    t_iter = iter(t_loader)
+    for i in range(test_01_abs.shape[0]//142):
+        data = t_iter.next()
+        data = data.cuda()
         output = neural_network(data)
         print(output.shape)
+
+
 #%%
-speech_spectogram = get_hadamard_product_result(torch.tensor(test_01), test_01_abs, output)
+# speech_spectogram = get_hadamard_product_result(test_01, np.abs(test_01), output.numpy())
+spec = (test_01 / np.abs(test_01)) * output.cpu().numpy().T
+#%%
+
+#%%
+
+#%%
+spec_istft = librosa.istft(spec, length=512, win_length=1024)
 
 
+#%%
+librosa.output.write_wav('test_s_01_recons.wav', spec_istft, sr)
 
-
-
-
-
-
-
-
-
-
-
-
-
+#%%
+librosa.output.write_wav('test_s_01_recons.wav', spec_istft, sr)
